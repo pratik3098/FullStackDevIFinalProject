59b1a6d7ef6d91c2c450fcaeace3ea3f95f14f90
Pratik Patil <pppatil@yorku.ca>
2019-09-21 21:59:50 -0400

    user login JSON databse added

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-
+let fs=require('fs');
+function jsonReader(filePath, cb) {
+    fs.readFile(filePath, (err, fileData) => {
+        if (err) {
+            return cb && cb(err)
+        }
+        try {
+            let object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        } catch(err) {
+            return cb && cb(err);
+        }
+    })
+}
+jsonReader('./database.json', (err, customer) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    customer.forEach(element => {
+        console.log(JSON.stringify(element)); // => "Infinity Loop Drive"
+    });
+   
+})
+console.log("Name:" + customers[0].name);
 function showFields(){
 let response={
       
@@ -7,6 +31,7 @@ let response={
         balance: "100"
 };
 //document.getElementById("public-key").innerHTML=response.key;
+
 document.getElementById("name").innerHTML=response.name;
 document.getElementById("balance").innerHTML=response.balance;
 
--- ethers-script.js
+++ ethers-script.js
@@ -1,17 +1,4 @@
 let ethers = require('ethers');
-let fs=require('fs');
-
-fs.readFile("database.json",'utf8',(err,data)=>{
-    if(err) throw err;
-    try{
-        let user= JSON.parse(data);
-        user=JSON.stringify(user);
-        console.log(user);
-    }
-    catch(err){
-        console.log("Error parsing JSON string");
-    }
-});
 
 
 let privateKey="489E52A1A043A2245BCA8426E36DF84DD6D5B4B4FC6D91C7268135857BA887EB";

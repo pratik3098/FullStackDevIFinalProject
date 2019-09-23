fa817811c49cbd9176fcf1b0a32d3a36d9b870b4
Pratik Patil <pppatil@yorku.ca>
2019-09-23 17:23:17 -0400

    POST request added

--- src/app.js
+++ src/app.js
@@ -1,21 +1,22 @@
 const path=require('path');
 const fs=require('fs');
-const bodyParser= require('body-parser');
 const express= require('express');
+const bodyParser= require('body-parser');
 const app = express();
 
 app.set('title','Ether Wallet');
 app.set('view engine','hbs');
 app.set('views',path.join(__dirname,"../views"));
 //app.use(express.static("./"); 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.get('',(req,res)=>{
     //res.sendFile("index.html",{root: __dirname});
     res.render("index",{data: " "});
 })
 app.post('/submit',function(req,res){
-    console.log(req.body);
-     res.render('index',{data: "Hello World;"})
+    console.log(req.query);
+     res.render('index',{data: "Hello World"})
 });
 
 app.listen(8080,()=>{

b67fef16b6fb00a007d5d910cbf6fc0f97bbab75
Pratik Patil <101284380@georgebrown.ca>
2019-09-24 17:21:29 -0400

    Data inputted from form

--- src/app.js
+++ src/app.js
@@ -1,23 +1,21 @@
-const path=require('path');
-const fs=require('fs');
-const express= require('express');
+const path=require('path')
+const fs=require('fs')
+const express= require('express')
 const bodyParser= require('body-parser');
-const app = express();
-
-app.set('title','Ether Wallet');
-app.set('view engine','hbs');
+const app = express()
+app.set('title','Ether Wallet')
+app.set('view engine','hbs')
 app.set('views',path.join(__dirname,"../views"));
-//app.use(express.static("./"); 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+app.use(express.static("../views")); 
+app.use(bodyParser.urlencoded({extended: false}));
 app.get('',(req,res)=>{
     //res.sendFile("index.html",{root: __dirname});
     res.render("index",{data: " "});
 })
-app.post('/submit',function(req,res){
-    console.log(req.query);
+app.post('/submit',(req, res)=>{
+     console.log(JSON.stringify(req.body));
      res.render('index',{data: "Hello World"})
-});
+})
 
 app.listen(8080,()=>{
     console.log("Server is running on port: 8080");

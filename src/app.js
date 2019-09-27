a7de16581da7e5d206341753be2a9f097852275e
pratik3098 <pratik3098@gmail.com>
2019-09-27 13:24:54 -0400

    Script update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const path=require('path')
+const ethersj= require('./ethers-script')
 const fs=require('fs')
 const express= require('express')
 const bodyParser= require('body-parser');
@@ -7,9 +8,11 @@ app.set('title','Ether Wallet')
 app.set('view engine','hbs')
 app.set('views',path.join(__dirname,"../views"))
 app.use(express.static("../views"))
+ethersj.createWallet()
 app.use(bodyParser.urlencoded({extended: false}))
 app.get('',(req,res)=>{
     //res.sendFile("index.html",{root: __dirname})
+    ethersj.createWallet()
     res.render("index",{bool: "hidden", data: "<h1> Hello World!</h1>"})
 })
 
--- src/ethers-script.js
+++ src/ethers-script.js
@@ -1,5 +1,5 @@
 let ethers = require('ethers')
-let etherString
+/* let etherString
 try {
   let provider = ethers.getDefaultProvider('rinkeby')
   let apiToken = "U2ADDD8KDD731FIGBJ4DM4SKNYWA2TKVHW"
@@ -7,11 +7,12 @@ try {
   let wallet = new ethers.Wallet(privateKey, provider)
   let etherscanProvider = new ethers.providers.EtherscanProvider('rinkeby',apiToken)
    etherscanProvider.getBalance(wallet.address).then((balance) => {
-
-    etherString = ethers.utils.formatEther(balance)
-
+    console.log(balance)
+    etherString=ethers.utils.formatEther(balance)
+    console.log("Balance: " + etherString);
+    
 })
- // console.log("Balance: " + etherString);
+  console.log("Balance: " + etherString);
   console.log(wallet.address)
 
  // console.log(wallet)
@@ -21,8 +22,9 @@ catch (err) {
   console.log(JSON.stringify(err))
 }
 
-
-function sendTranscation(address, amount) {
+*/
+exports.sendTranscation = function (address, amount) {
+ if(amount<=balance)
   amount = ethers.utils.parseEther(amount)
   let tx = {
     to: address,
@@ -36,17 +38,34 @@ function sendTranscation(address, amount) {
 
 
 
-function createWallet(publicKey, privateKey) {
-  let key;
-  if (privateKey)
-    key = privateKey
-  else if (publicKey)
-    key = publicKey
-  mywallet = {
-    "publicKey": wallet.signingKey.keyPair.publicKey,
-    "privateKey": wallet.signingKey.keyPair.privateKey,
-    "balance": " "
-
-
-  }
+async function createWallet() {
+let ethers = require('ethers')
+let etherString
+try {
+  let provider = ethers.getDefaultProvider('rinkeby')
+  let apiToken = "U2ADDD8KDD731FIGBJ4DM4SKNYWA2TKVHW"
+  let privateKey = "43D57D2EEAC30F7F172042747F79830356AFDFAD43D22BEF5C1C1BA5239C9ED0"
+  let wallet = new ethers.Wallet(privateKey, provider)
+  let etherscanProvider = new ethers.providers.EtherscanProvider('rinkeby',apiToken)
+  const balance = ethers.utils.formatEther(await  etherscanProvider.getBalance(wallet.address))
+    console.log("Balance 111: " + balance);
+ // console.log(wallet)
 }
+catch (err) {
+  // console.log("Error Occured")
+  console.log(JSON.stringify(err))
+}
+ return {
+    data: "Hello World!",
+    name: "George Brown"
+ }
+}
+
+async function test()
+{
+
+  const k = await createWallet();
+  console.log(k);
+}
+
+test();
